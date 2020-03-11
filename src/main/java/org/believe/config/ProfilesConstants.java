4d73a9369755d6620054fc47f4b503591d49f76b
1619kHz <1517262320@qq.com>
2020-03-11 15:36:50 +0800

    设置环境常量

---
+++ src/main/java/org/believe/config/ProfilesConstants.java
@@ -0,0 +1,7 @@
+package org.believe.config;
+
+public interface ProfilesConstants {
+  String SPRING_PROFILE_DEVELOPMENT = "dev";
+  String SPRING_PROFILE_PRODUCTION = "prod";
+  String SPRING_PROFILE_CLOUD = "cloud";
+}
